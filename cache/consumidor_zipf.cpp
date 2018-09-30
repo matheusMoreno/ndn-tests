1e85154998860595fa345e6748df161d70d62842
Matheus Moreno <matheus.moreno.08@gmail.com>
2018-09-29 22:10:41 -0300

    Update consumidor_zipf.cpp

--- cache/consumidor_zipf.cpp
+++ cache/consumidor_zipf.cpp
@@ -35,15 +35,18 @@ double medias[NUMERO_DIVISOES] = {0.0};
 int qtdePedidos[NUMERO_DIVISOES] = {0};
 bool success = false;
 
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<> dis(0.0, 1.0);
+
 int zipf(double alpha, int n)
 {
   static int first = TRUE;      // Static first time flag
   static double c = 0;          // Normalization constant
-  static double *sum_probs;     // Pre-calculated sum of probabilities
   double z;                     // Uniform random number (0 < z < 1)
-  int zipf_value;               // Computed exponential value to be returned
+  double sum_prob;              // Sum of probabilities
+  double zipf_value;            // Computed exponential value to be returned
   int    i;                     // Loop counter
-  int low, high, mid;           // Binary-search bounds
 
   // Compute normalization constant on first call only
   if (first == TRUE)
@@ -51,12 +54,6 @@ int zipf(double alpha, int n)
     for (i=1; i<=n; i++)
       c = c + (1.0 / pow((double) i, alpha));
     c = 1.0 / c;
-
-    sum_probs = malloc((n+1)*sizeof(*sum_probs));
-    sum_probs[0] = 0;
-    for (i=1; i<=n; i++) {
-      sum_probs[i] = sum_probs[i-1] + c / pow((double) i, alpha);
-    }
     first = FALSE;
   }
 
@@ -68,18 +65,16 @@ int zipf(double alpha, int n)
   while ((z == 0) || (z == 1));
 
   // Map z to the value
-  low = 1, high = n, mid;
-  do {
-    mid = floor((low+high)/2);
-    if (sum_probs[mid] >= z && sum_probs[mid-1] < z) {
-      zipf_value = mid;
+  sum_prob = 0;
+  for (i=1; i<=n; i++)
+  {
+    sum_prob = sum_prob + c / pow((double) i, alpha);
+    if (sum_prob >= z)
+    {
+      zipf_value = i;
       break;
-    } else if (sum_probs[mid] >= z) {
-      high = mid-1;
-    } else {
-      low = mid+1;
     }
-  } while (low <= high);
+  }
 
   // Assert that zipf_value is between 1 and N
   assert((zipf_value >=1) && (zipf_value <= n));
